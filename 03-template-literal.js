2b1dbfa8371ca970e6e6aa83ca1440b82a138cb7
André Marques <andre.m.d.abreu@gmail.com>
2023-11-22 08:49:05 -0300

    Create 03-template-literal.js

---
+++ 03-template-literal.js
@@ -0,0 +1,13 @@
+// 03-template-literal.js
+
+const nome = "Chapolin";
+const sobrenome = "Colorado";
+let idade = 25;
+let cidade = "São Paulo";
+let estado = "SP";
+
+/* Concatenação tradicional usando operador +
+Meu nome é Chapolin Colorado, tenho 25 anos e moro atualmente na cidade de São Paulo - SP. */
+let mensagem1 = "Meu nome é "+nome+" "+sobrenome+", tenho "+idade+" anos e moro atualmente na cidade de "+cidade+" - "+estado+".";
+
+console.log(mensagem1);
