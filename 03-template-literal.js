6efc048899476ab8a689ff6482fac758da39d013
André Marques <andre.m.d.abreu@gmail.com>
2023-11-22 09:08:58 -0300

    Update 03-template-literal.js

--- 03-template-literal.js
+++ 03-template-literal.js
@@ -8,6 +8,12 @@ let estado = "SP";
 
 /* Concatenação tradicional usando operador +
 Meu nome é Chapolin Colorado, tenho 25 anos e moro atualmente na cidade de São Paulo - SP. */
-let mensagem1 = "Meu nome é "+nome+" "+sobrenome+", tenho "+idade+" anos e moro atualmente na cidade de "+cidade+" - "+estado+".";
+let mensagem1 = "Meu nome é <b class=\"destaque\">"+nome+"</b> "+sobrenome+", tenho "+idade+" anos e moro atualmente na cidade de "+cidade+" - "+estado+".";
 
 console.log(mensagem1);
+console.log("-------");
+
+/* Template Literal/String */
+let mensagem2 = `Meu nome é ${nome.toUpperCase()} ${sobrenome}, tenho ${idade} anos e moro atualmente na cidade de ${cidade} - ${estado}. Sou ${idade >= 18 ? 'maior' : 'menor'} de idade.`;
+
+console.log(mensagem2);
