05a4f2c080b387c6252e89e7aa3dfd3939047865
André Marques <andre.m.d.abreu@gmail.com>
2023-11-22 11:53:21 -0300

    Update 05-operador-spread.js

--- 05-operador-spread.js
+++ 05-operador-spread.js
@@ -10,3 +10,20 @@ const maisBandas = [...bandas, "Slayer", "Nightwish"];
 console.log(bandas);
 console.log(maisBandas);
 console.log(maisBandas[0]);
+
+// Spread com objetos
+const cliente = {
+    nome: "Seu Madruga",
+    cidade: "São Paulo",
+    idade: 55
+};
+
+const novosDados = {
+    ...cliente, // Copiando as props. do cliente pra cá
+    estado: "SP",
+    pedido: "123xyz",
+    total: 5000.88,
+    idade: 60 // sobreescrevendo valores de propriedades
+};
+
+console.log(novosDados);
