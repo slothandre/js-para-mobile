1953b613c5d194d8518e3bd87e5753f492281dcc
André Marques <andre.m.d.abreu@gmail.com>
2023-11-22 11:30:35 -0300

    Create funcoes.js

---
+++ modulos/funcoes.js
@@ -0,0 +1,18 @@
+// modulos/funcoes.js
+
+function converteMaiusculas(texto) {
+    return texto.toUpperCase();
+}
+
+function converteMinusculas(texto) {
+    return texto.toLowerCase();
+}
+
+function formataMoeda(valor) {
+    return valor.toLocaleString("pt-br", {
+        style: "currency",
+        currency: "BRL"
+    });
+}
+
+export {converteMaiusculas, converteMinusculas, formataMoeda};
