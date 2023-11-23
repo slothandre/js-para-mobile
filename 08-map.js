3c12d1f068261fd9f17e2ffe4b78934e223ba0db
André Marques <andre.m.d.abreu@gmail.com>
2023-11-23 11:43:13 -0300

    Exemplo de map

---
+++ 08-map.js
@@ -0,0 +1,34 @@
+// 08-map.js
+import cursos from "./modulos/cursos.js";
+
+/* map (mapear,mapeia - transformar em outra coisa)
+Passa por elementos de um array e realiza operações em
+cada um deles através de uma função (callback), gerando um novo
+array. */
+
+// Exemplo 1
+const numeros = [10, 20, 5, 12, 66, 50];
+console.log(numeros);
+
+const numerosDobrados = numeros.map(numero => numero * 2);
+console.log(numerosDobrados);
+
+console.log(cursos);
+
+// Exemplo 2: gerar um novo array apenas com os nomes dos cursos
+const titulos = cursos.map(curso => curso.titulo);
+console.log(titulos);
+
+/* Exercício
+Use o map para gerar um novo array APENAS com os preços
+calculados com um desconto de 10%.
+
+Como calcular?
+-Opção 1: valor - valor * 0.10
+-Opção 2: valor * 0.9
+
+E mostre no console. */
+
+const valoresBlackFriday = cursos.map(curso => curso.preco * 0.9);
+console.log(valoresBlackFriday);
+
