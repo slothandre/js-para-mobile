8eef6e147feebf2dce7068c6f6e7f4cac59ff80c
André Marques <andre.m.d.abreu@gmail.com>
2023-11-24 10:51:02 -0300

    Update 10-reduce.js

--- 10-reduce.js
+++ 10-reduce.js
@@ -1,1 +1,7 @@
 // 10-reduce.js
+
+/* reduce (reduzir, redução)
+Passa por elementos de um array (usando uma função
+callback com parâmetros, e um parâmetro com valor inicial)
+e retorna um único valor/resultado. Pode ser útil para operações
+matemáticas que geram um único resultado */
