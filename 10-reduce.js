fc753ab7612d439eb41aa898601396309febc479
André Marques <andre.m.d.abreu@gmail.com>
2023-11-24 10:59:49 -0300

    Update 10-reduce.js

--- 10-reduce.js
+++ 10-reduce.js
@@ -5,3 +5,12 @@ Passa por elementos de um array (usando uma função
 callback com parâmetros, e um parâmetro com valor inicial)
 e retorna um único valor/resultado. Pode ser útil para operações
 matemáticas que geram um único resultado */
+
+// Exemplo 1
+const valores = [10, 5, 50, 200, 1000];
+const total = valores.reduce((acumulador, valor) => {
+    return acumulador + valor;
+}, 0);
+
+console.log(valores);
+console.log(total);
